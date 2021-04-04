f9648d199f5c5e6e628e8f297f6736304433f08e
d-rigel <alozie4God@gmail.com>
2021-04-04 14:41:12 -0700

    config file update

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://emmanuel:@alozie444@cluster2.z9o3q.mongodb.net/passportLogin?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://emmanuel:mypassword@cluster2.z9o3q.mongodb.net/passportLogin?retryWrites=true&w=majority"
 }
